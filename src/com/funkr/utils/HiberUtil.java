9536ffed5d970cc4e082145150f29e1fd0d7b951
Ryan Fitzgerald <fitz297@gmail.com>
2012-11-13 07:50:20 -0500

    refactoring web svc utils

---
+++ src/com/funkr/utils/HiberUtil.java
@@ -0,0 +1,26 @@
+package com.funkr.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HiberUtil {
+
+	private static final SessionFactory sessionFactory;
+
+	static {
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			sessionFactory = new Configuration().configure()
+					.buildSessionFactory();
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+}
